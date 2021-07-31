52d29de1e5dab991a01c4a380103e45358e5f661
Pericles001 <periclesadjovi@outlook.fr>
2021-07-31 05:43:10 +0100

    finish dicesimulator

--- 1-dice/dicesimulator.py
+++ 1-dice/dicesimulator.py
@@ -1,61 +1,67 @@
 import random
 
-number=random.randint(1, 6)
 
 
-if number == 1:
-    print("---------")
-    print("|       |")
-    print("|   0   |")
-    print("|       |")
-    print("---------")
-
-elif number == 2:
-    print("---------")
-    print("|   0   |")
-    print("|   0   |")
-    print("|       |")
-    print("|       |")
-    print("|       |")
-    print("---------")
-    
-
-elif number == 3:
-    print("---------")
-    print("|   0   |")
-    print("|   0   |")
-    print("|   0   |")
-    print("|       |")
-    print("|       |")
-    print("---------")
-    
-
-elif number == 4:
-    print("---------")
-    print("|       |")
-    print("| 0   0 |")
-    print("|       |")
-    print("| 0   0 |")
-    print("|       |")
-    print("---------")
+x="y"
 
 
-elif number == 5:
-    print("---------")
-    print("|       |")
-    print("| 0   0 |")
-    print("|   0   |")
-    print("| 0   0 |")
-    print("|       |")
-    print("---------")
-
-else:
-    
-    print("---------")
-    print("|       |")
-    print("| 0 0 0 |")
-    print("|       |")
-    print("| 0 0 0 |")
-    print("|       |")
-    print("---------")
+while x=="y":
+  number=random.randint(1, 6)
+  if number == 1:
+      print("---------")
+      print("|       |")
+      print("|   0   |")
+      print("|       |")
+      print("---------")
+  
+  elif number == 2:
+      print("---------")
+      print("|   0   |")
+      print("|   0   |")
+      print("|       |")
+      print("|       |")
+      print("|       |")
+      print("---------")
+      
+  
+  elif number == 3:
+      print("---------")
+      print("|   0   |")
+      print("|   0   |")
+      print("|   0   |")
+      print("|       |")
+      print("|       |")
+      print("---------")
+      
+  
+  elif number == 4:
+      print("---------")
+      print("|       |")
+      print("| 0   0 |")
+      print("|       |")
+      print("| 0   0 |")
+      print("|       |")
+      print("---------")
+  
+  
+  elif number == 5:
+      print("---------")
+      print("|       |")
+      print("| 0   0 |")
+      print("|   0   |")
+      print("| 0   0 |")
+      print("|       |")
+      print("---------")
+  
+  else:
+      
+      print("---------")
+      print("|       |")
+      print("| 0 0 0 |")
+      print("|       |")
+      print("| 0 0 0 |")
+      print("|       |")
+      print("---------")
+  
+  x = input("Do you want to play again? (y/n)")
 
