3e37eb93a5f1072d10e8a530544a5fb6445d18b1
Pericles001 <periclesadjovi@outlook.fr>
2021-08-05 14:10:52 +0100

    user moves

--- 4-ticTacToe/main.py
+++ 4-ticTacToe/main.py
@@ -40,6 +40,24 @@ def game():
                        b[3] == l and b[5] == l and b[7] == l) or (b[1] == l and b[4] == l and b[7] == l) or (
                        b[2] == l and b[5] == l and b[8] == l) or (b[3] == l and b[6] == l and b[9] == l)
 
+    def plzyerMove():
+        run = True
+        while run:
+            move = input("Please, select a position to enter the X between 1 to 9")
+            try:
+                move = int(move)
+                if move > 0 and move < 10:
+                    if spaceIsFree(move):
+                        run = False
+                        insertLetter('X', move)
+                    else:
+                        print('Sorry, this space is occupied')
+                else:
+                    print('Please, type a number between 1 and 9')
+
+            except:
+                print('Please, type a number')
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
