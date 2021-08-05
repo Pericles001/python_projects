455da9bbc2d99cb2f5e07b8f934d35b6a85cd53c
Pericles001 <periclesadjovi@outlook.fr>
2021-08-05 01:59:28 +0100

    add new project

---
+++ decrement/main.py
@@ -0,0 +1,31 @@
+# This is a sample Python script.
+
+# Press Maj+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+# Write
+# a
+# program
+# that
+# repeatedly
+# prompts
+# a
+# user
+# for integer numbers until the user enters 'done'.Once 'done' is entered,
+# print out the largest and smallest of the numbers.If the user enters anything
+# other than a valid number catch it with a try / except and put out an appropriate
+# message and ignore the number.Enter 7, 2, bob, 10, and 4 and match the output below.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
