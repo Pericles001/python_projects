d4f2706a0ce59c9fe91ee2ff12bfb102e0dff01f
Pericles001 <periclesadjovi@outlook.fr>
2021-08-05 12:58:57 +0100

    finish decrement function

--- decrement/main.py
+++ decrement/main.py
@@ -4,7 +4,6 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-
 # Write
 # a
 # program
@@ -18,14 +17,41 @@
 # other than a valid number catch it with a try / except and put out an appropriate
 # message and ignore the number.Enter 7, 2, bob, 10, and 4 and match the output below.
 
+def prompt():
+    print(
+        "Hello , you should enter each time an integer till you enter done, then we will provide you the maximal value and the minimal value")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    user_min = 0
+    user_max = 0
+    user_array = []
+    user_input = ""
+
+    user_input = input("Enter a number, enter done if you want to stop")
+    user_min = int(user_input)
+    while user_input != "done":
+        print(user_input)
+        user_array += user_input
+        if int(user_input) < user_min:
+            user_min = int(user_input)
+            print("all new min is :", user_min)
+        if int(user_input) > user_max:
+            user_max = int(user_input)
+            print("all new max is :", user_max)
+        user_input = input("Enter a number, enter done if you want to stop")
+        if user_input == "done":
+            print("over")
+
+            print("min value is ", user_min)
+            print("max value is :", user_max)
+            break
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    prompt()
+else:
+    pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
