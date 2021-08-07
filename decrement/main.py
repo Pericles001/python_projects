ca91cdd0b349e9d9efc4ff29989ddf7b2c0a053e
Pericles001 <periclesadjovi@outlook.fr>
2021-08-07 21:50:08 +0100

    add decrement update

--- decrement/main.py
+++ decrement/main.py
@@ -18,33 +18,35 @@
 # message and ignore the number.Enter 7, 2, bob, 10, and 4 and match the output below.
 
 def prompt():
-    print(
-        "Hello , you should enter each time an integer till you enter done, then we will provide you the maximal value and the minimal value")
+    try:
+        print(
+            "Hello , you should enter each time an integer till you enter done, then we will provide you the maximal value and the minimal value")
+        user_min = 0
+        user_max = 0
+        user_array = []
+        user_input = ""
 
-    user_min = 0
-    user_max = 0
-    user_array = []
-    user_input = ""
-
-    user_input = input("Enter a number, enter done if you want to stop")
-    user_min = int(user_input)
-    while user_input != "done":
-        print(user_input)
-#       user_array += user_input
-        if int(user_input) < user_min:
-            user_min = int(user_input)
-            print("all new min is :", user_min)
-        if int(user_input) > user_max:
-            user_max = int(user_input)
-            print("all new max is :", user_max)
         user_input = input("Enter a number, enter done if you want to stop")
-        if user_input == "done":
-            print("over")
+        user_min = int(user_input)
+        while user_input != "done":
+            print(user_input)
+            #       user_array += user_input
+            if int(user_input) < user_min:
+                user_min = int(user_input)
+                print("all new min is :", user_min)
+            if int(user_input) > user_max:
+                user_max = int(user_input)
+                print("all new max is :", user_max)
+            user_input = input("Enter a number, enter done if you want to stop")
+            if user_input == "done":
+                print("over")
 
-            print("min value is ", user_min)
-            print("max value is :", user_max)
-            break
+                print("min value is ", user_min)
+                print("max value is :", user_max)
+                break
 
+    except:
+        print("Error, you should enter a number")
 
 
 # Press the green button in the gutter to run the script.
@@ -54,4 +56,3 @@ else:
     pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
