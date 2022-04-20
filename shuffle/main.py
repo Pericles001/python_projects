782b5470d2e71e2c379f27c91807299790e7ca01
pericles001 <periclesadjovi@outlook.fr>
2022-04-20 16:20:59 +0100

    Created items getter method and started shuffle method

---
+++ shuffle/main.py
@@ -0,0 +1,30 @@
+# tHIS IS THE MAIN FILE PROGRAM TO SHUFFLE ITEMS
+from random import shuffle
+
+items = []
+
+def get_items():
+    """ Get items to shuffle as   a list"""
+    print('Enter items to shuffle, one per line.\n')
+    print("Enter 'done' when finished.\n")
+    item = input("Item: ")
+    if item == "done":
+        return items
+    while item != "done":
+        items.append(item)
+        item = input("Item: ")
+    return items
+
+
+def shuffle_items():
+    """Shuffle items which where taken"""
+    print("Shuffling items...")
+    print("Shuffled items: {}".format(shuffle(items)))
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    get_items()
+    shuffle_items()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
