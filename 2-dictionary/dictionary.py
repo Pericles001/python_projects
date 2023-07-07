2bc74fd1ef2911ee2cc2d32467cce31ed17977b9
Pericles001 <periclesadjovi@gmail.com>
2023-07-07 18:51:56 +0200

    Edit file to read json dictionary per line

--- 2-dictionary/dictionary.py
+++ 2-dictionary/dictionary.py
@@ -1,5 +1,14 @@
-
 import json
 
-data = json.load(open("data.json"))
-print(data)
+
+def get_line():
+    try:
+        #I want to print first line of json file
+        data = [json.loads(line)
+                for line in open('data.json', 'r', encoding='utf-8')]
+        print(data[0])
+    except Exception as e:
+        print(e)
+
+
+get_line()
