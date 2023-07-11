333b4db1f47c2366d4f4df87500070ecea4b7da2
Pericles001 <periclesadjovi@gmail.com>
2023-07-11 16:03:17 +0200

    Edit code : - Review save_line method to save dictionary data into a temporary file
    - Edit take_argument to take user input as word

--- 2-dictionary/dictionary.py
+++ 2-dictionary/dictionary.py
@@ -1,14 +1,37 @@
 import json
 
 
-def get_line():
+def save_line():
+    """
+    Method to save each line of dictionary in a file
+    :return:
+    """
     try:
-        #I want to print first line of json file
-        data = [json.loads(line)
-                for line in open('data.json', 'r', encoding='utf-8')]
-        print(data[0])
+        with open('data.json', 'r', encoding='utf-8') as file:
+            first_line = file.readline()
+            data = json.loads(first_line)
+            # store datas in a file and replace ',' with '\n'
+            with open('data.txt', 'w', encoding='utf-8') as file:
+                for key, value in data.items():
+                    line = f"{key}: {value}\n"
+                    file.write(line)
+    except FileNotFoundError:
+        print("File 'data.json' not found.")
+    except json.JSONDecodeError:
+        print("Unable to decode the JSON data in the first line.")
+    except Exception as e:
+        print("An error occurred:", e)
+
+
+def take_argument(target_word):
+    """
+    this function will take a word as argument
+
+    :return:
+    """
+    try:
+        print("Enter a word: \m")
+        target_word = input()
+        return target_word
     except Exception as e:
         print(e)
-
-
-get_line()
