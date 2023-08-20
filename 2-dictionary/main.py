b78d2df1d6f8c3bf298bd3d837f227185ac6f860
Pericles001 <periclesadjovi@gmail.com>
2023-08-20 16:21:28 +0200

    Edit file : - Add conditional statement to check user inputs and choices

--- 2-dictionary/main.py
+++ 2-dictionary/main.py
@@ -5,12 +5,34 @@ Python code that simulates a dictionary based on a json file and online API.
 data.json is a json file that contains a dictionary of words and their meanings.
 """
 
-from b_online_dictionary.requests import take_argument, search_word,internet_checker, redirect_to_offline
+from b_online_dictionary.requests import internet_checker, \
+    search_word_online
+from a_offline_dictionary.dictionary import redirect_to_offline, take_argument, save_line
 
 if __name__ == "__main__":
-    if internet_checker():
-        target_word = take_argument()
-        result = search_word(target_word)
-        print(result)
-    else:
-        redirect_to_offline()
+    save_line()
+    while True:
+        if internet_checker():
+            target_word = take_argument("Welcome to the online dictionary. Enter a word or type 'exit' to quit:")
+
+            if target_word.lower() == 'exit':
+                print("Exiting the dictionary.")
+                break
+
+            result = search_word_online(target_word)
+
+            if result:
+                print("Definitions:")
+                for meaning in result:
+                    part_of_speech = meaning["partOfSpeech"]
+                    print(f"{part_of_speech.capitalize()}:")
+
+                    for definition_data in meaning["definitions"]:
+                        definition = definition_data["definition"]
+                        print(f" - {definition}")
+            else:
+                print("No definitions found online for the word. You can try searching offline.")
+                redirect_to_offline()
+        else:
+            print("You are offline. Redirecting to offline dictionary.")
+            redirect_to_offline()
